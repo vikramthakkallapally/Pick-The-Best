a7dca6519239fa6caa9cf2d23f923ef14c5923de
vaidyapusainikhil <89408610+vaidyapusainikhil@users.noreply.github.com>
2022-06-02 17:41:04 -0500

    Updated the RoleService.java file.

--- e-com/springboot/e-com/src/main/java/com/price/comparision/ecom/service/RoleService.java
+++ e-com/springboot/e-com/src/main/java/com/price/comparision/ecom/service/RoleService.java
@@ -2,7 +2,6 @@ package com.price.comparision.ecom.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.price.comparision.ecom.dao.RoleDao;
 import com.price.comparision.ecom.entity.Role;
 
@@ -11,9 +10,7 @@ public class RoleService {
 	
 	@Autowired
 	private RoleDao roleDao;
-	
 	public Role createNewRole(Role role) {
 		return roleDao.save(role);
 	}
-
 }
