3a2fa39346ef91d7b4cdb537f5f40d33d92b838c
Deepthi Kaithi <89281610+deepthi39@users.noreply.github.com>
2022-09-04 22:37:36 -0500

    Add files via upload

---
+++ e-com/springboot/LoginApplication.java
@@ -0,0 +1,84 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApplication.class, args);
+	}
+
+}
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+public class User {
+	
+
+	private String userName;
+	
+	private String userFirstName;
+	
+	private String userLastName;
+	
+	private String userPassword;
+	
+	private String email;
+    public User() {
+		
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+	public String getUserFirstName() {
+		return userFirstName;
+	}
+
+
+	public void setUserFirstName(String userFirstName) {
+		this.userFirstName = userFirstName;
+	}
+
+
+	public String getUserLastName() {
+		return userLastName;
+	}
+
+
+	public void setUserLastName(String userLastName) {
+		this.userLastName = userLastName;
+	}
+
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
