d84017a7ca088d1394054f2ce3d6971326e66b25
Deepthi Kaithi <89281610+deepthi39@users.noreply.github.com>
2022-09-07 09:16:53 -0500

    Update User.java

--- e-com/springboot/User.java
+++ e-com/springboot/User.java
@@ -1,12 +1,8 @@
 import java.util.Set;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@Entity
 public class User {
 	
-	@Id
+
 	private String userName;
 	
 	private String userFirstName;
