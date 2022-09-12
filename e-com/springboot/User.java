14c49f1f3f1b61eb21bd24f6a7abf4b57a02dbd0
Lakshmi Sireesha Pavalla <lakshmisireeshapavalla@gmail.com>
2022-09-11 19:56:06 -0500

    Updated user

--- e-com/springboot/User.java
+++ e-com/springboot/User.java
@@ -1,8 +1,19 @@
+package com.price.comparision.ecom.bean;
+
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
+
+@Entity
 public class User {
 	
-
+	@Id
 	private String userName;
 	
 	private String userFirstName;
@@ -12,7 +23,19 @@ public class User {
 	private String userPassword;
 	
 	private String email;
-public User() {
+	
+	@ManyToMany(fetch=FetchType.EAGER, cascade= CascadeType.ALL)
+	@JoinTable(name="USER_ROLE", 
+			joinColumns = { 
+					@JoinColumn(name = "user_id") 
+						}, 
+			inverseJoinColumns = { 
+					@JoinColumn(name = "role_id") 
+						})
+	private Set<Role> roles;
+	
+	
+	public User() {
 		
 	}
 
@@ -53,6 +76,16 @@ public User() {
 
 	public void setUserPassword(String userPassword) {
 		this.userPassword = userPassword;
+	}
+
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
 	}
 
 	public String getEmail() {
